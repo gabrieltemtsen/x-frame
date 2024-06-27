052417f5010c7b37df7330832a7424bfda0bd502
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-06-27 12:15:11 +0100

    debug

--- src/pages/api/frame/get-tx-frame.ts
+++ src/pages/api/frame/get-tx-frame.ts
@@ -9,7 +9,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     if(req.method !== "POST") {
-        return res.status(405).json({error: "Method not allowed"});
+        return res.status(405).json({error: "Method not alloweded"});
     }
 
     const {isValid, message} = await CoinbaseKit.validateMessage(req.body as FrameRequest) ;
